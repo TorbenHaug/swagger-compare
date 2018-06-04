d2e076763667d0ce6d4b69d32d943941af33eef7
TorbenHaug <claustorbenhaug@hotmail.de>
2018-06-04 19:21:01 +0200

    add links and mediatypes tests

---
+++ swagger-compare/swagger-compare-core/src/test/Java/de/haug_dev/swagger_compare/swagger_compare_core/media/MediaTypesCompareHolderTest.java
@@ -0,0 +1,184 @@
+package de.haug_dev.swagger_compare.swagger_compare_core.media;
+
+import de.haug_dev.swagger_compare.swagger_compare_core.CompareHolderFactory;
+import de.haug_dev.swagger_compare.swagger_compare_datatypes.*;
+import io.swagger.v3.oas.models.media.MediaType;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class MediaTypesCompareHolderTest {
+    @Test
+    public void compareNullNull() {
+        CompareHolderFactory compareHolderFactory = new CompareHolderFactory();
+        CompareHolderFactory spiedCompareHolderFactory = spy(compareHolderFactory);
+
+        Map<String, MediaType> leftMap = null;
+        Map<String, MediaType> rightMap = null;
+
+        NodeCompareResult expected = new NodeCompareResult(CompareCriticalType.WARNING, CompareCriticalType.CRITICAL);
+
+        ICompareResult actual = spiedCompareHolderFactory.getMediaTypesCompareHolder().compare(leftMap, rightMap, CompareCriticalType.WARNING,CompareCriticalType.CRITICAL);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareNullEmptyMap() {
+        CompareHolderFactory compareHolderFactory = new CompareHolderFactory();
+        CompareHolderFactory spiedCompareHolderFactory = spy(compareHolderFactory);
+
+        Map<String, MediaType> leftMap = null;
+        Map<String, MediaType> rightMap = new HashMap<>();
+
+        NodeCompareResult expected = new NodeCompareResult(CompareCriticalType.WARNING, CompareCriticalType.CRITICAL);
+
+        ICompareResult actual = spiedCompareHolderFactory.getMediaTypesCompareHolder().compare(leftMap, rightMap, CompareCriticalType.WARNING,CompareCriticalType.CRITICAL);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareEmptyMapNull() {
+        CompareHolderFactory compareHolderFactory = new CompareHolderFactory();
+        CompareHolderFactory spiedCompareHolderFactory = spy(compareHolderFactory);
+
+        Map<String, MediaType> leftMap = new HashMap<>();
+        Map<String, MediaType> rightMap = null;
+
+        NodeCompareResult expected = new NodeCompareResult(CompareCriticalType.WARNING, CompareCriticalType.CRITICAL);
+
+        ICompareResult actual = spiedCompareHolderFactory.getMediaTypesCompareHolder().compare(leftMap, rightMap, CompareCriticalType.WARNING,CompareCriticalType.CRITICAL);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareEmptyMapEmptyMap() {
+        CompareHolderFactory compareHolderFactory = new CompareHolderFactory();
+        CompareHolderFactory spiedCompareHolderFactory = spy(compareHolderFactory);
+
+        Map<String, MediaType> leftMap = new HashMap<>();
+        Map<String, MediaType> rightMap = new HashMap<>();
+
+        NodeCompareResult expected = new NodeCompareResult(CompareCriticalType.WARNING, CompareCriticalType.CRITICAL);
+
+        ICompareResult actual = spiedCompareHolderFactory.getMediaTypesCompareHolder().compare(leftMap, rightMap, CompareCriticalType.WARNING,CompareCriticalType.CRITICAL);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareCreated() {
+        CompareHolderFactory compareHolderFactory = new CompareHolderFactory();
+        CompareHolderFactory spiedCompareHolderFactory = spy(compareHolderFactory);
+        MediaTypeCompareHolder subCompareHolder = mock(MediaTypeCompareHolder.class);
+        when(spiedCompareHolderFactory.getMediaTypeCompareHolder()).thenReturn(subCompareHolder);
+
+        Map<String, MediaType> leftMap = new HashMap<>();
+        MediaType subLeft = null;
+        Map<String, MediaType> rightMap = new HashMap<>();
+        MediaType subRight = new MediaType();
+        rightMap.put("test", subRight);
+
+        ILeafCompareResult expectedLeaf = new LeafCompareResult(subLeft, subRight, CompareResultType.CREATED, CompareCriticalType.INFO);
+
+        when(subCompareHolder.compare(eq(subLeft), eq(subRight), any(), any())).thenReturn(expectedLeaf);
+
+        NodeCompareResult expected = new NodeCompareResult(CompareCriticalType.WARNING, CompareCriticalType.CRITICAL);
+        expected.put("test", expectedLeaf);
+
+        ICompareResult actual = spiedCompareHolderFactory.getMediaTypesCompareHolder().compare(leftMap, rightMap, CompareCriticalType.WARNING,CompareCriticalType.CRITICAL);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareDeleted() {
+        CompareHolderFactory compareHolderFactory = new CompareHolderFactory();
+        CompareHolderFactory spiedCompareHolderFactory = spy(compareHolderFactory);
+        MediaTypeCompareHolder subCompareHolder = mock(MediaTypeCompareHolder.class);
+        when(spiedCompareHolderFactory.getMediaTypeCompareHolder()).thenReturn(subCompareHolder);
+
+        Map<String, MediaType> leftMap = new HashMap<>();
+        MediaType subLeft = new MediaType();
+        leftMap.put("test", subLeft);
+        Map<String, MediaType> rightMap = new HashMap<>();
+        MediaType subRight = null;
+
+        ILeafCompareResult expectedLeaf = new LeafCompareResult(subLeft, subRight, CompareResultType.DELETED, CompareCriticalType.INFO);
+
+        when(subCompareHolder.compare(eq(subLeft), eq(subRight), any(), any())).thenReturn(expectedLeaf);
+
+        NodeCompareResult expected = new NodeCompareResult(CompareCriticalType.WARNING, CompareCriticalType.CRITICAL);
+        expected.put("test", expectedLeaf);
+
+        ICompareResult actual = spiedCompareHolderFactory.getMediaTypesCompareHolder().compare(leftMap, rightMap, CompareCriticalType.WARNING,CompareCriticalType.CRITICAL);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareUnchanged() {
+        CompareHolderFactory compareHolderFactory = new CompareHolderFactory();
+        CompareHolderFactory spiedCompareHolderFactory = spy(compareHolderFactory);
+        MediaTypeCompareHolder subCompareHolder = mock(MediaTypeCompareHolder.class);
+        when(spiedCompareHolderFactory.getMediaTypeCompareHolder()).thenReturn(subCompareHolder);
+
+        Map<String, MediaType> leftMap = new HashMap<>();
+        MediaType subLeft = new MediaType();
+        leftMap.put("test", subLeft);
+
+        Map<String, MediaType> rightMap = new HashMap<>();
+        MediaType subRight = new MediaType();
+        rightMap.put("test", subRight);
+
+        ILeafCompareResult expectedLeaf = new LeafCompareResult(subLeft, subRight, CompareResultType.UNCHANGED, CompareCriticalType.NONE);
+
+        when(subCompareHolder.compare(eq(subLeft), eq(subRight), any(), any())).thenReturn(expectedLeaf);
+
+        NodeCompareResult expected = new NodeCompareResult(CompareCriticalType.WARNING, CompareCriticalType.CRITICAL);
+        expected.put("test", expectedLeaf);
+
+        ICompareResult actual = spiedCompareHolderFactory.getMediaTypesCompareHolder().compare(leftMap, rightMap, CompareCriticalType.WARNING,CompareCriticalType.CRITICAL);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareChanged() {
+        CompareHolderFactory compareHolderFactory = new CompareHolderFactory();
+        CompareHolderFactory spiedCompareHolderFactory = spy(compareHolderFactory);
+        MediaTypeCompareHolder subCompareHolder = mock(MediaTypeCompareHolder.class);
+        when(spiedCompareHolderFactory.getMediaTypeCompareHolder()).thenReturn(subCompareHolder);
+
+        Map<String, MediaType> leftMap = new HashMap<>();
+        MediaType subLeft = new MediaType();
+        subLeft.setExample("Test1");
+        leftMap.put("test", subLeft);
+
+        Map<String, MediaType> rightMap = new HashMap<>();
+        MediaType subRight = new MediaType();
+        subRight.setExample("Test2");
+        rightMap.put("test", subRight);
+
+        ILeafCompareResult expectedLeaf = new LeafCompareResult(subLeft, subRight, CompareResultType.CHANGED, CompareCriticalType.INFO);
+
+        when(subCompareHolder.compare(eq(subLeft), eq(subRight), any(), any())).thenReturn(expectedLeaf);
+
+        NodeCompareResult expected = new NodeCompareResult(CompareCriticalType.WARNING, CompareCriticalType.CRITICAL);
+        expected.put("test", expectedLeaf);
+
+        ICompareResult actual = spiedCompareHolderFactory.getMediaTypesCompareHolder().compare(leftMap, rightMap, CompareCriticalType.WARNING,CompareCriticalType.CRITICAL);
+
+        assertEquals(expected, actual);
+    }
+}
